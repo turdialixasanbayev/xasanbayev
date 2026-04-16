03c8068987a313164af9cb853b213171652ca8ee
Turdiali <xasanbayevturdiali92@gmail.com>
2026-04-16 20:00:29 +0500

    prod

--- config/urls.py
+++ config/urls.py
@@ -15,27 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include, re_path
-
-from django.conf import settings
-from django.conf.urls.static import static
-from django.views.static import serve
-
-from django.conf.urls import handler404
+from django.urls import path, include
 
 urlpatterns = [
     path('xasanbayev-admin-panel/', admin.site.urls),
 
     path('', include('apps.contact.urls')),
     path('', include('apps.article.urls')),
-    path('', include('apps.portfolio.urls')),
-
-    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
-    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+    path('', include('apps.portfolio.urls'))
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 handler404 = 'config.errors.custom_404'
